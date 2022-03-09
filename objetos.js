a7deea7cd40f35e8af74b098c2400258dd2b677a
ndavidj <ndavidj@hotmail.com.ar>
2022-03-09 16:27:57 -0300

    Se agrego archivo objetos

---
+++ objetos.js
@@ -0,0 +1,22 @@
+let funcionesDeTareas = require("./funcionesDeTareas");
+
+// Modificar esta variable para cambiar la acción a realizar por el switch
+let accion = "listar";
+
+switch (accion) {
+  case "listar":
+  
+    let tareas = funcionesDeTareas.leerArchivo();
+    tareas.forEach(function (tarea) {
+      console.log(tarea);
+      } 
+      
+    );
+    break;
+  case "":
+    console.log("Atención - Tienes que pasar una acción.");
+    break;
+  default:
+    console.log("No entiendo qué quieres hacer");
+    break;
+}
